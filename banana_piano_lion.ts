b122c2fd40a92b002caab6b9f033dc24c8d7c850
michvich <136633240+michvich@users.noreply.github.com>
2023-07-17 07:41:43 +0700

    Refactor code for maintainability

---
+++ banana_piano_lion.ts
@@ -0,0 +1,105 @@
+1. class BreakingBarriers {
+2   constructor(public message: string) {}    
+3 
+4   public welcomeMessage(): string {
+5     return `Welcome! ${this.message}`;
+6   }
+7 
+8   public breakingBarriers(): string {
+9     return `Let's break barriers and achieve our goals!`;
+10  }
+11 
+12  public goodbyeMessage(): string {
+13    return `Goodbye! ${this.message}`;
+14  }
+15 
+16  public greetingsMessage(): string {
+17    return `Greetings! ${this.message}`;
+18  }
+19 
+20  public congratulationsMessage(): string {
+21    return `Congratulations! ${this.message}`;
+22  }
+23 
+24  public celebrateSuccess(): string {
+25    return `Let's celebrate our successes!`;
+26  }
+27 
+28  public goOneStepFurther(): string {
+29    return `Let's go one step further than we ever thought possible!`;
+30  }
+31 
+32  public acceptChallenges(): string {
+33    return `Let's accept challenges and push our limits!`;
+34  }
+35 
+36  public takeAction(): string {
+37    return `Let's take action and make our dreams come true!`;
+38  }
+39 
+40  public reachForTheStars(): string {
+41    return `Let's reach for the stars and never give up!`;
+42  }
+43 
+44  public trustYourself(): string {
+45    return `Let's trust ourselves and believe that anything is possible!`;
+46  } 
+47 
+48  public showCompassion(): string {
+49    return `Let's show compassion and understanding towards everyone we meet!`;
+50  } 
+51 
+52  public neverGiveUp(): string {
+53    return `Let's never give up no matter what obstacles stand in our way!`;
+54  } 
+55 
+56  public createChange(): string {
+57    return `Let's create change and make the world a better place!`;
+58  } 
+59 
+60  public beOpenMinded(): string {
+61    return `Let's be open-minded and never stop learning!`;
+62  } 
+63 
+64  public neverStopDreaming(): string {
+65    return `Let's never stop dreaming and reach for our goals!`;
+66  } 
+67 
+68  public embraceOpportunities(): string {
+69    return `Let's embrace opportunities and break barriers!`;
+70  }
+71 
+72  public stayPositive(): string {
+73    return `Let's stay positive and face any challenge head-on!`;
+74  }
+75 
+76  public liveLifeToTheFullest(): string {
+77    return `Let's live life to the fullest and never give up on our ambitions!`;
+78  }
+79 
+80  public spreadLove(): string {
+81    return `Let's spread love and kindness in everything we do!`;
+82  }
+83 
+84  public fosterInnovation(): string {
+85    return `Let's foster innovation and be the change we want to see!`;
+86  }
+87 
+88  public beFearless(): string {
+89    return `Let's be fearless and empower others along our journey!`;
+90  }
+91 
+92  public stayHopeful(): string {
+93    return `Let's stay hopeful and always believe in ourselves!`;
+94  }
+95 
+96  public embraceTheUnknown(): string {
+97    return `Let's embrace the unknown and take risks for growth!`;
+98  }
+99
+100 public makeAChange(): string {
+101   return `Let's make a change and break barriers together!`;
+102 }
+}
+
+export default BreakingBarriers;
