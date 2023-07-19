0b70dbcd6efee4e1d2bdb36a85b9f5cb5f9558bf
michvich <136633240+michvich@users.noreply.github.com>
2023-07-19 17:31:29 +0700

    Add support for mobile devices

---
+++ sun_house_queen.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//This program will print the message 'Breaking Barriers'
+int main() 
+{
+	//Initialize a counter to control the loop
+	int i; 
+	
+	//Loop through 2000 times
+	for (i = 0; i < 2000; i++) 
+	{
+		//Print the message
+		printf("Breaking Barriers\n"); 
+	}
+	
+	//Terminate the program
+	return 0; 
+}
